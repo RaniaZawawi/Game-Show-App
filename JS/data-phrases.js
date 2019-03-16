002374da0b2ed007dfbec845d73c69d3d734d72e
Rania <34710804+RaniaZawawi@users.noreply.github.com>
2019-03-16 08:55:12 +0300

    create phrases
    created an array of phrases in a seperate javascript file

---
+++ JS/data-phrases.js
@@ -0,0 +1,12 @@
+const phrases = [
+                  "We are the world",
+                  "I believe I can fly",
+                  "Do not repeat yourself",
+                  "Be a rainbow in someone else s cloud",
+                  "Listen if you want to be heard",
+                  "I have to act to live",
+                  "One day or day one you decide",
+                  "nothing is impossible",
+                  "I can I will watch me",
+                  "Life is better when you are laughing"
+];
